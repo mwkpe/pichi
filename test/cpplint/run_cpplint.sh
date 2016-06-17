f13392ab89d9fb4f35c5b436b4e98c20526e09c3
peter <xe@kthx.de>
2016-06-17 03:13:59 +0200

    Initial commit of cpplint files

---
+++ test/cpplint/run_cpplint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+./cpplint.py \
+--linelength=100 \
+--filter=\
+-build/include,\
+-build/header_guard,\
+-build/c++11,\
+-whitespace/braces,\
+-whitespace/indent,\
+-whitespace/newline,\
+-whitespace/braces,\
+-readability/todo,\
+-readability/braces,\
+-legal/copyright \
+$(find ../.. -name \*.h -or -name \*.cpp | \
+grep -vE "doctest|gsl-lite|mainwindow\.|\/test")
